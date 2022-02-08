62caff1d49f086577eeb896925f5f44183ca3010
alazyula <alazyula1@gmail.com>
2022-02-08 04:11:36 +0300

    first commit

---
+++ cakma-yutup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cakma-yutup
+//
+//  Created by macbookaır on 8.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
